e754c3deb8f2dc4f8e728ac0b9911e47855b858b
AJ_Lu <aojunlu@stu.scu.edu.cn>
2024-10-01 14:18:08 +0800

    Update memo_arch_craft.py

--- class_il/convs/memo_arch_craft.py
+++ class_il/convs/memo_arch_craft.py
@@ -149,7 +149,7 @@ def get_arch_craft(pretrained=False, progress=True, args=None, **kwargs):
     pool = [int(x) for x in args['pool']]
     double = [int(x) for x in args['double']]
 
-    border = int(depth*3/4)
+    border = int(depth*2/3)
 
     basenet = GeneralizedNet(depth, width, copy.deepcopy(pool), copy.deepcopy(double), border, args)
     adaptivenet = SpecializedNet(depth, basenet.out_dim, copy.deepcopy(pool), copy.deepcopy(double), border, args)
